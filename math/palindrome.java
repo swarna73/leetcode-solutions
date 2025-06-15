cfa03cfe911b564398b50c1228709da5a09bcc5f
Swarnalatha Swaminathan <110965638+swarna73@users.noreply.github.com>
2025-06-15 21:25:44 +0200

    Add Palindrome Number solution (String + Math)

---
+++ math/palindrome.java
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/palindrome-number/
+// Level: Easy
+// Tags: Math, String
+// Date Solved: 15 June 2025
+
+class Solution {
+    public boolean isPalindrome(int x) {
+// Negative numbers and numbers ending in 0 (except 0 itself) are not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        String str = Integer.toString(x);
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
