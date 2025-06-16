b59c0f218be09c7fbd2e39dc5b20e17fffd15c47
Swarnalatha Swaminathan <110965638+swarna73@users.noreply.github.com>
2025-06-16 10:02:34 +0200

    Update two_sum_brute.java

--- arrays/two_sum_brute.java
+++ arrays/two_sum_brute.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] two_sum_brute(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
