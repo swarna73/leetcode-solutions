bbcaa0c0710f26792edf286bbe6fcf1531b3f3c8
Swarnalatha Swaminathan <110965638+swarna73@users.noreply.github.com>
2025-06-18 08:08:19 +0200

    Add binary numbers
    Example 1:
    
    Input: a = "11", b = "1"
    Output: "100"
    Example 2:
    
    Input: a = "1010", b = "1011"
    Output: "10101"

---
+++ strings/add_binary.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int bitA = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int bitB = (j >= 0) ? b.charAt(j--) - '0' : 0;
+
+            int sum = bitA + bitB + carry;
+            result.append(sum % 2);       
+            carry = sum / 2;              
+        }
+
+        return result.reverse().toString(); 
+    }
+}
