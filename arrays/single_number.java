7ffcd96a35c5430260cac21f618729d4647b16bf
Swarnalatha Swaminathan <110965638+swarna73@users.noreply.github.com>
2025-06-24 09:08:07 +0200

    Single Number
    Input: nums = [2,2,1]
    
    Output: 1
    
    Example 2:
    
    Input: nums = [4,1,2,1,2]
    
    Output: 4

---
+++ arrays/single_number.java
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+          int result = 0;
+
+        for (int num : nums) {
+            result ^= num; // XOR cancels out duplicates
+        }
+
+        return result;
+    }
+};
