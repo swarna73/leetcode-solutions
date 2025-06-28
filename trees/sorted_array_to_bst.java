98205afa9b3db21e903d742d70a5844162890f11
Swarnalatha Swaminathan <110965638+swarna73@users.noreply.github.com>
2025-06-28 22:51:16 +0200

    sorted_array_to_bst.java
    Input: nums = [-10,-3,0,5,9]
    Output: [0,-3,9,-10,null,5]
    Explanation: [0,-10,5,null,-3,null,9] is also accepted:

---
+++ trees/sorted_array_to_bst.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) return null;
+
+        int mid = left + (right - left) / 2;
+
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildBST(nums, left, mid - 1);
+        node.right = buildBST(nums, mid + 1, right);
+
+        return node;
+    }
+}
