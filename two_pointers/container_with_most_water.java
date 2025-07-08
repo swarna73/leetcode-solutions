69131e0e2a5ca156fc3dd6543990e5d667c27229
Swarnalatha Swaminathan <110965638+swarna73@users.noreply.github.com>
2025-07-08 12:03:39 +0200

    Create container_with_most_water.java

---
+++ two_pointers/container_with_most_water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+              int maxArea = 0;
+        int left = 0, right = height.length - 1;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+            maxArea = Math.max(maxArea, area);
+
+            // Move the shorter line inward
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;  
+    }
+}
